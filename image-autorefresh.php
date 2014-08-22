566ddaa2110cce7367525b16dcda0c51e5081934
Klaske van Vuurden <klaske.van.vuurden@uit.no>
2014-08-22 12:34:24 +0200

    Generate image ids with random strings to make it possible to have multiple shortcodes on one page

--- image-autorefresh.php
+++ image-autorefresh.php
@@ -10,15 +10,7 @@ Copyright: Klaske van Vuurden
 */
 
 
-
 defined('ABSPATH') or die("Don't access directly!");
-
-
-/* input: shortcode with keywords src and refresh_time
- *
- * output: div containing image + jquery that reloads the image in interval refresh_time
- *
- */
 
 //[image-autorefresh src="image-url" refresh_time=number_of_seconds caption=text align=left]
 function image_autorefresh_shortcode($atts){
@@ -29,7 +21,9 @@ function image_autorefresh_shortcode($atts){
 			'align' => 'left',
 	), $atts );
 
-   /* first version: only supports one image per page */
+   /* second version: allows multiple images per page with random string */
+
+   $randomString = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6);
 
 	ob_start();
 	?>
@@ -37,17 +31,21 @@ function image_autorefresh_shortcode($atts){
 	    setInterval(function()
         {
             d = new Date();
-            image = jQuery('img.image_autorefresh');
+            image = jQuery('img#image_autorefresh_<?php echo $randomString ?>');
             image.attr("src", image.data("src") + "?" + d.getTime());
         }, <?php echo $a['refresh_time']; ?>000);
 	</script>
 
 	<div class="<?php echo '' != $a['caption'] ? 'wp-caption ': ''; ?>align<?php echo $a['align']; ?>">
-	<img src=" <?php echo $a['src']; ?>" data-src=" <?php echo $a['src']; ?>" class="image_autorefresh" data-refresh="<?php echo $a['refresh_time']; ?>">
+	<img src=" <?php echo $a['src']; ?>" data-src=" <?php echo $a['src']; ?>"
+	     class="image_autorefresh" id="image_autorefresh_<?php echo $randomString ?>"
+	     data-refresh="<?php echo $a['refresh_time']; ?>">
 	<?php echo '' != $a['caption'] ? '<p>' . $a['caption'] . '</p>' : ''; ?>
 	</div>
 	<?php
 	return ob_get_clean();
+
+
 
 
 }
