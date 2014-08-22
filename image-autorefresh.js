f7ba7be6fddea31492dc15befb9ea4558888efaa
Klaske van Vuurden <klaske.van.vuurden@uit.no>
2014-08-22 12:07:38 +0200

    Added jquery to refresh on regular interval + added caption and align attributes

---
+++ image-autorefresh.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kvu022 on 8/22/14.
+ */
+
+jQuery(document).ready(function(){
+
+   jQuery('img.image_autorefresh').width(jQuery('#content').width());
+
+
+});
--- image-autorefresh.php
+++ image-autorefresh.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Image autorefresh shortcode
-Plugin URI: http://site.uit.no
+Plugin URI: https://github.com/klasske/wp_image_autorefresh
 Description: Small shortcode plugin created specifically for reloading images for site.uit.no/spaceweather
 Version: 1
 Author: Klaske van Vuurden
-Author URI: http://no.linkedin.com/in/klaskevanvuurden/
+Author URI: https://github.com/klasske/
 Copyright: Klaske van Vuurden
 */
 
@@ -20,15 +20,44 @@ defined('ABSPATH') or die("Don't access directly!");
  *
  */
 
-//[image-autorefresh src="image-url" refresh_time=number_of_seconds]
+//[image-autorefresh src="image-url" refresh_time=number_of_seconds caption=text align=left]
 function image_autorefresh_shortcode($atts){
 	$a = shortcode_atts( array(
-			'src' => $atts['src'],
-			'refresh_time' => 'something else',
+			'src' => includes_url() . 'images/wlw/wp-watermark.png',
+			'refresh_time' => 60,
+			'caption' => '',
+			'align' => 'left',
 	), $atts );
-	
-	return 'This is a test...' . $a['src'];
+
+   /* first version: only supports one image per page */
+
+	ob_start();
+	?>
+	<script>
+	    setInterval(function()
+        {
+            d = new Date();
+            image = jQuery('img.image_autorefresh');
+            image.attr("src", image.data("src") + "?" + d.getTime());
+        }, <?php echo $a['refresh_time']; ?>000);
+	</script>
+
+	<div class="<?php echo '' != $a['caption'] ? 'wp-caption ': ''; ?>align<?php echo $a['align']; ?>">
+	<img src=" <?php echo $a['src']; ?>" data-src=" <?php echo $a['src']; ?>" class="image_autorefresh" data-refresh="<?php echo $a['refresh_time']; ?>">
+	<?php echo '' != $a['caption'] ? '<p>' . $a['caption'] . '</p>' : ''; ?>
+	</div>
+	<?php
+	return ob_get_clean();
+
+
 }
+
+//enqueue script (if not already there)
+function image_autorefresh_scripts() {
+    wp_enqueue_script("jquery-image-autorefresh", plugins_url("/image-autorefresh.js", __FILE__ )  , array('jquery', 'jquery-ui-core'));
+}
+
+add_action( 'wp_enqueue_scripts', 'image_autorefresh_scripts' );
 add_shortcode('image-autorefresh', 'image_autorefresh_shortcode');
 
 
